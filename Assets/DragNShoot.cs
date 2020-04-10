d7effba6d558f1cad9c5799b0e30b86b9b8c45f1
Choi Jin Young <choijy613@gmail.com>
2020-04-11 02:54:21 +0900

    0411

---
+++ Assets/DragNShoot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragNShoot : MonoBehaviour
+{
+    public float power = 10f;
+    public Rigidbody2D rb;
+
+    public Vector2 minPower;
+    public Vector2 maxPower;
+
+    Camera cam;
+    Vector2 force;
+    Vector3 startPoint;
+    Vector3 endPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            startPoint.z = 0;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            endPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            endPoint.z = 0;
+
+            force = new Vector2(Mathf.Clamp(startPoint.x - endPoint.x, minPower.x, maxPower.x), Mathf.Clamp(startPoint.y - endPoint.y, minPower.y, maxPower.y));
+            rb.AddForce(force * power, ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    private float timer = 0f;
+    public float spawnDelay;
+
+    public GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            Vector2 spawnPos = new Vector2(Random.Range(-2f, 2f), 6.5f);
+            Instantiate(enemy, spawnPos, transform.rotation);
+            timer = spawnDelay;
+        }
+    }
+}

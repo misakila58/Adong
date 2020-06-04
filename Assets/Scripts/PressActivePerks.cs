73ed8d4cbcd1eebf85100ad4fe6d78726dc41090
Choi Jin Young <choijy613@gmail.com>
2020-06-04 17:27:09 +0900

    0604 액티브스킬 쿨타임 UI 구현, 0531리소스 적용

--- Assets/Scripts/PressActivePerks.cs
+++ Assets/Scripts/PressActivePerks.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PressActivePerks : MonoBehaviour
 {
@@ -9,11 +10,24 @@ public class PressActivePerks : MonoBehaviour
 
     private ActivePerks activePerks;
 
+    public int perkNum; // 1 = Sword  2 = trap  3 = laser
+    public Image background;
+
     void Start()
     {
         crossbow = GameObject.FindGameObjectWithTag("Crossbow");
         firePoint = GameObject.FindGameObjectWithTag("FirePoint");
         activePerks = crossbow.GetComponent<ActivePerks>();
+    }
+
+    void Update()
+    {
+        if (perkNum == 1)
+            background.fillAmount = activePerks.swordCurCoolDown / activePerks.swordCoolDown;
+        else if (perkNum == 2)
+            background.fillAmount = activePerks.trapCurCoolDown / activePerks.trapCoolDown;
+        else if (perkNum == 3)
+            background.fillAmount = activePerks.laserCurCoolDown / activePerks.laserCoolDown;
     }
 
     public void SwordSwing()

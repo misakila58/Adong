5ecdc3cda2b3d095e90d89fb682977338f5b0df0
666rat666 <nuriwahanul0101@gmail.com>
2020-06-30 23:17:26 +0900

    3

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void SceneChange1()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void SceneChange2()
+    {
+        SceneManager.LoadScene("scene2");
+    }
+    public void SceneChange3()
+    {
+        SceneManager.LoadScene("scene3");
+    }
+    public void SceneChange4()
+    {
+        SceneManager.LoadScene("scene4");
+    }
+}
